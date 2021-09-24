ce04f2f5efb5b3b832fa57ce3a6c32591a26d02d
Dangerred <msfreire52@gmail.com>
2021-09-24 01:24:25 -0300

    Update Exerc.073 - Exercitando Módulos em Python.py

--- Atividades - Python/modulação/Exerc.073 - Exercitando Módulos em Python.py
+++ Atividades - Python/modulação/Exerc.073 - Exercitando Módulos em Python.py
@@ -1,7 +1,7 @@
 import moeda   
     
 preço = float(input('Digite o preço: R$'))
-print(f'A metade do valor colocado é {moeda.metade(preço, False)}.')
-print(f'O dobro do valor colocado é {moeda.moeda(moeda.dobro(preço, True))}.')
-print(f'Esse valor aumentado em 10% resulta em {moeda.moeda(moeda.porcentagem(preço, 10, True))}.')
-print(f'Esse valor reduzido em 12% resulta em {moeda.moeda(moeda.redução(preço, 12, True))}.\n')
+print(f'A metade do valor colocado é {moeda.metade(preço, True)}.')
+print(f'O dobro do valor colocado é {moeda.dobro(preço, True)}.')
+print(f'Esse valor aumentado em 10% resulta em {moeda.porcentagem(preço, 10, True)}.')
+print(f'Esse valor reduzido em 12% resulta em {moeda.redução(preço, 12, True)}.\n')
