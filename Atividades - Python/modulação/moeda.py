381bb72766273187a13beafbaa769e6800f2c838
Dangerred <msfreire52@gmail.com>
2021-09-24 01:59:00 -0300

    Redefinição
    Mudança para encurtamento do programa principal e adicionada nova função.

--- Atividades - Python/modulação/moeda.py
+++ Atividades - Python/modulação/moeda.py
@@ -1,25 +1,25 @@
-def metade(num, c=False):
+def metade(num, c=True):
     valor = num/2
     if c == True:
         return moeda(valor)
     else:
         return valor
 
-def dobro(num, c=False):
+def dobro(num, c=True):
     valor = 2*num
     if c == True:
         return moeda(valor)
     else:
         return valor
     
-def porcentagem(num, p, c=False):
+def porcentagem(num, p, c=True):
     valor = (p+100)/100*num
     if c == True:
         return moeda(valor)
     else:
         return valor
 
-def redução(num, p, c=False):
+def redução(num, p, c=True):
     valor = (100-p)/100*num
     if c == True:
         return moeda(valor)
@@ -28,3 +28,9 @@ def redução(num, p, c=False):
 
 def moeda(valor):
     return f'R${valor:>.2f}'.replace('.',',')
+
+def resumo(preço, a, b):
+    print(f'A metade do valor colocado é {metade(preço)}.')
+    print(f'O dobro do valor colocado é {dobro(preço)}.')
+    print(f'Esse valor aumentado em {a}% resulta em {porcentagem(preço, a)}.')
+    print(f'Esse valor reduzido em {b}% resulta em {redução(preço, b)}.')
