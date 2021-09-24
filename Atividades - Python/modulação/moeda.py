d63449000df5a27f55332f05f3cc0ad26fb7a687
Dangerred <msfreire52@gmail.com>
2021-09-24 01:21:59 -0300

    nova modificação

--- Atividades - Python/modulação/moeda.py
+++ Atividades - Python/modulação/moeda.py
@@ -1,18 +1,30 @@
-def metade(num):
+def metade(num, c=False):
     valor = num/2
-    return valor
+    if c == True:
+        return moeda(valor)
+    else:
+        return valor
 
-def dobro(num):
+def dobro(num, c=False):
     valor = 2*num
-    return valor
-
-def porcentagem(num, p):
+    if c == True:
+        return moeda(valor)
+    else:
+        return valor
+    
+def porcentagem(num, p, c=False):
     valor = (p+100)/100*num
-    return valor
+    if c == True:
+        return moeda(valor)
+    else:
+        return valor
 
-def redução(num, p):
+def redução(num, p, c=False):
     valor = (100-p)/100*num
-    return valor
+    if c == True:
+        return moeda(valor)
+    else:
+        return valor
 
 def moeda(valor):
-    return f'R${valor:.2f}'.replace('.',',')
+    return f'R${valor:>.2f}'.replace('.',',')
